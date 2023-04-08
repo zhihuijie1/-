9dae2a3073bae974ad0e38d7da7e97ef4d1de7b4
西雅图的雪很美 <92995461+zhihuijie1@users.noreply.github.com>
2023-04-08 21:13:10 +0800

    2023.4.8

--- ReversePair.java
+++ ReversePair.java
@@ -1,21 +1,23 @@
 package algorithmbasic.class5;
 
-// 数组小和
-public class SmallSum {
-    public static int smallSum(int[] arr) {
+/*逆序对
+在一个数组中，任何一个前面的数a，和任何一个后面的数b，如果(a,b)是降序的，就称为降序对
+给定一个数组arr，求数组的降序对总数量
+ */
+public class ReversePair {
+    public static int reverPairNumber(int[] arr) {
         if (arr == null || arr.length < 2) {
             return 0;
         }
         return process(arr, 0, arr.length - 1);
     }
 
-    // 将arr[L , R] 排有序， 并返回这一范围上的数组小和的值
+    // 在arr[L , R]上排好序，并返回逆序对数量
     public static int process(int[] arr, int L, int R) {
         if (L == R) {
             return 0;
         }
         int mid = L + ((R - L) >> 1);
-
         return
                 process(arr, L, mid)
                 +
@@ -29,46 +31,39 @@ public class SmallSum {
         int p1 = L;
         int p2 = mid + 1;
         int i = 0;
-        int sum = 0;
+        int count = 0;
         while (p1 <= mid && p2 <= R) {
-            /*
-            if (arr[p1] < arr[p2]) {
-                sum += arr[p1] * (R - p2 + 1);
-                help[i++] = arr[p1++];
-            } else if (arr[p1] == arr[p2]) {
-                help[i++] = arr[p2++];
-            } else {
-                help[i++] = arr[p2++];
-            }
-            */
-            sum += arr[p1] < arr[p2] ? arr[p1] * (R - p2 + 1) :  0;
-            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+            // 倒序排列
+            count += arr[p2] < arr[p1] ? (R - p2 + 1) : 0;
+            help[i++] = arr[p2] < arr[p1] ? arr[p1++] : arr[p2++];
         }
-        while(p1 <= mid) {
+
+        while (p1 <= mid) {
             help[i++] = arr[p1++];
         }
-        while(p2 <= R) {
+
+        while (p2 <= R) {
             help[i++] = arr[p2++];
         }
+
         for (int j = 0; j < help.length; j++) {
-            arr[L + j] = help[j];
+            arr[j + L] = help[j];
         }
-        return sum;
+
+        return count;
     }
 
-    // for test------
     // for test
     public static int comparator(int[] arr) {
-        if (arr == null || arr.length < 2) {
-            return 0;
-        }
-        int res = 0;
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 0; j < i; j++) {
-                res += arr[j] < arr[i] ? arr[j] : 0;
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    ans++;
+                }
             }
         }
-        return res;
+        return ans;
     }
 
     // for test
@@ -127,18 +122,18 @@ public class SmallSum {
         int testTime = 500000;
         int maxSize = 100;
         int maxValue = 100;
-        boolean succeed = true;
+        System.out.println("测试开始");
         for (int i = 0; i < testTime; i++) {
             int[] arr1 = generateRandomArray(maxSize, maxValue);
             int[] arr2 = copyArray(arr1);
-            if (smallSum(arr1) != comparator(arr2)) {
-                succeed = false;
+            if (reverPairNumber(arr1) != comparator(arr2)) {
+                System.out.println("Oops!");
                 printArray(arr1);
                 printArray(arr2);
                 break;
             }
         }
-        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+        System.out.println("测试结束");
     }
 
 }
